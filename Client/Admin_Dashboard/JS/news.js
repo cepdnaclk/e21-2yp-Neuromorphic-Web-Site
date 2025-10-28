01a38fa60b61570d92670e986b43758f48d96f48
Sandeep Y.P. <e21353@eng.pdn.ac.lk>
2025-10-28 10:39:13 +0530

    implement dynamic news table rendering with edit and delete actions

--- Client/Admin_Dashboard/JS/news.js
+++ Client/Admin_Dashboard/JS/news.js
@@ -24,3 +24,47 @@ function removeImage(event) {
   removeBtn.style.display = "none";
   fileInput.value = "";
 }
+
+// News functions
+function updateNewsTable(newsData) {
+  const tableBody = document.querySelector("#newsTable tbody");
+  let html = "";
+
+  if (newsData.length > 0) {
+    newsData.forEach((news) => {
+      const date = new Date(news.date).toLocaleDateString();
+      const imageSrc = news.image ? news.image : "./uploads/news.jpg";
+      html += `
+            <tr>
+                <td><img src="${imageSrc}" alt="News Image" class="news-thumb" /></td>
+                <td>${news.title}</td>
+                <td>${news.author}</td>
+                <td>${date}</td>
+                <td>${news.brief}</td>
+                <td>${
+                  news.link && news.link !== "#"
+                    ? `<a href="${news.link}" target="_blank">View</a>`
+                    : "-"
+                }</td>
+                <td>
+                    <div class="action-buttons">
+                          <button class="table-btn btn-edit" onclick='openNewsModal(${JSON.stringify(
+                            news
+                          ).replace(/'/g, "&apos;")})'>
+                            <img src="./img_dashbord/table/edit.svg" class="edit-icon"/>
+                        </button>
+                        <button class="table-btn btn-delete" onClick="deleteItem('${
+                          news._id
+                        }','news',updateNewsTable)">
+                            <img src="./img_dashbord/table/delete.svg" class="delete-icon"/>
+                        </button>
+                    </div>
+                </td>
+            </tr>`;
+    });
+  } else {
+    html = '<tr><td colspan="7" class="text-center">No news found.</td></tr>';
+  }
+
+  tableBody.innerHTML = html;
+}
