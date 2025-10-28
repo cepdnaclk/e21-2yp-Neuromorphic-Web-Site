d618dcc5487b935cb0b8e481ec00c35d08eb1a86
Jayasundara J.M.S.D.B. <e21198@eng.pdn.ac.lk>
2025-10-28 09:59:58 +0530

    Configure publication routes with public reads and private CRUD

--- Server/router/publications.js
+++ Server/router/publications.js
@@ -2,3 +2,14 @@ const express=require('express');
 const router=express.Router();
 const {getPublication,getPublications,deletePublication,updatePublication,createPublication}=require('../controller/publication');
 
+const {protect,authorize}=require('../middleware/auth');
+
+router.route('/')
+                .get(getPublications)
+                .post(protect,authorize('Admin','Lecture'),createPublication)
+
+router.route('/:id')
+                .delete(protect,authorize('Admin','Lecture'),deletePublication)
+                .put(protect,authorize('Admin','Lecture'),updatePublication)
+                .get(getPublication)
+module.exports=router;
