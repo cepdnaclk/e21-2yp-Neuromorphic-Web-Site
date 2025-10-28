8d42e136c38b87b989e56f32e3153dcd31d159f5
Jayasundara J.M.S.D.B. <e21198@eng.pdn.ac.lk>
2025-10-28 09:25:28 +0530

    Implement loadSectionData routing function for admin dashboard modules

--- Client/Admin_Dashboard/JS/main.js
+++ Client/Admin_Dashboard/JS/main.js
@@ -15,3 +15,30 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener('submit',handleLogin);
 
 });
+
+
+// Load data for a specific section
+function loadSectionData(section) {
+    switch (section) {
+        case "dashboard":
+            loadSection('admin/dashboard',loadDashboardData);
+            break;
+        case "contributors":
+            loadSection('contributors',updateContributorsTable);
+            break;
+        case "news":
+            loadSection('news',updateNewsTable);
+            break;
+        case "publication":
+            loadSection('publications',updatePublicationTable);
+            break;
+        case "projects":
+            loadSection('projects',updateProjectsTable);
+            break;
+        case "users":
+            loadSection('admin/users',updateUsersTable);
+            break;
+        case "blogs":
+            loadSection("blogs",updateBlogsTable);
+    }
+}
