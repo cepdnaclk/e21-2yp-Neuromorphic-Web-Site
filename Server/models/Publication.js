7bb8abee233de51bc063c566a0bbb01b03b5a25d
Jayasundara J.M.S.D.B. <e21198@eng.pdn.ac.lk>
2025-10-28 09:54:12 +0530

    Add authors relationship, journal, and year fields to PublicationSchema

--- Server/models/Publication.js
+++ Server/models/Publication.js
@@ -8,3 +8,26 @@ const PublicationSchema = new mongoose.Schema({
     trim: true,
     maxlength: [200, "Title cannot be more than 200 characters"],
   },
+
+  authors: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Contributor",
+      required: true,
+    },
+  ],
+  journal: {
+    type: String,
+    required: [true, "Please add publication venue"],
+    trim: true,
+    maxlength: [200, "Venue cannot be more than 200 characters"],
+  },
+  year: {
+    type: Number,
+    required: [true, "Please add publication year"],
+    min: [1900, "Year must be at least 1900"],
+    max: [
+      new Date().getFullYear() + 5,
+      "Year cannot be more than 5 years in the future",
+    ],
+  },
